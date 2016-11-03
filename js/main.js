5535a5bb0949339e220bf240a5c9b35b8c8bd2bd
Simion Padurean <simpakl@yahoo.com>
2016-11-03 13:08:21 -0700

    Pray

--- js/main.js
+++ js/main.js
@@ -1,7 +1,3 @@
-$(document).on("pagecreate",function(event){
-	$(window).on("orientationchange", function(event){
-		if(event.orientation == "landscape"){
-			alert("Landscape is not supported by this website, please switch to portrait");
-		}
-	});
+$( window ).on( "orientationchange", function( event ) {
+  $( "#orientation" ).text( "This device is in " + event.orientation + " mode!" );
 });
