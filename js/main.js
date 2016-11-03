dad0b6042844a1e859500a4dbb372be9d34bae57
Simion Padurean <simpakl@yahoo.com>
2016-11-03 13:00:36 -0700

    One more try
    I sure hope no one sees these commit messages.

--- js/main.js
+++ js/main.js
@@ -1,5 +1,7 @@
-$(window).on("orientationchange", function(event){
-	if(event.orientation == "landscape"){
-		alert("Landscape is not supported by this website, please switch to portrait");
-	}
+$(document).on("pagecreate",function(event){
+	$(window).on("orientationchange", function(event){
+		if(event.orientation == "landscape"){
+			alert("Landscape is not supported by this website, please switch to portrait");
+		}
+	});
 });
