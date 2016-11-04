d9dbf1f6b750019e4455822da3a7553da2068bb5
Simion Padurean <simpakl@yahoo.com>
2016-11-04 16:49:55 -0700

    jquery works!!

--- js/main.js
+++ js/main.js
@@ -1,22 +1,19 @@
+//Smooth scrolling example found from W3 schools
 $(document).ready(function(){
-  // Add smooth scrolling to all links
   $("a").on('click', function(event) {
 
-    // Make sure this.hash has a value before overriding default behavior
     if (this.hash !== "") {
-      // Prevent default anchor click behavior
+      
       event.preventDefault();
 
       // Store hash
       var hash = this.hash;
 
-      // Using jQuery's animate() method to add smooth page scroll
-      // The optional number (800) specifies the number of milliseconds it takes to scroll to the specified area
+      
       $('html, body').animate({
         scrollTop: $(hash).offset().top
       }, 800, function(){
    
-        // Add hash (#) to URL when done scrolling (default click behavior)
         window.location.hash = hash;
       });
     } // End if
@@ -24,26 +21,24 @@ $(document).ready(function(){
 });
 
 
-$(document).ready(function(){
-	(function(){
-		var fadeSpeed = 200, fadeTo = 0.5, topDistance = 30;
-		var topbarME = function() { $('#navigation').fadeTo(fadeSpeed, 1);} topbarML = function() {$('#navigation').fadeTo(fadeSpeed, fadeTo);};
-		var inside = false;
-		
-		$(window).scroll(function(){
-			position = $(window).scrollTop();
-			if(position > topDistance && !inside){
-				topbarML();
-				$('#navigation').bind('mouseenter', topbarME);
-				$('#navigation').bind('mouseleave',topbarML);
-				inside=true;
-			}
-			else if(position < topDistance){
-				topbarME();
-				$('#navigation').unbind('mouseenter', topbarME);
-				$('#navigation').unbind('mouseleave', topbarML);
-				inside=false;
-			}
-		});
-	})();
+$(window).scroll(function() {
+    if ($(window).scrollTop() > 75) {
+        $('#navigation').css("opacity", 0.6);
+    }
+    else {
+        $('#navigation').css("opacity", 1);
+    }
 });
+
+$(document).ready(function(){
+    $('#navigation').live("mouseover", function () {
+            $(this).css("opacity", 1);
+            });
+
+    $('#navigation').live("mouseleave", function () {
+        if($(window).scrollTop() > 75) { // this here
+            $(this).css("opacity", 0.6);
+        }
+    });
+
+})
