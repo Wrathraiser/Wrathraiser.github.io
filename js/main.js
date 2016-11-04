fea97ed38553ca08bef2b589540128d8c84b73b6
Simion Padurean <simpakl@yahoo.com>
2016-11-03 21:15:14 -0700

    Updated css

--- js/main.js
+++ js/main.js
@@ -1,3 +1,23 @@
-$( window ).on( "orientationchange", function( event ) {
-  $( "#orientation" ).text( "This device is in " + event.orientation + " mode!" );
+$(document).ready(function(){
+	(function(){
+		var fadeSpeed = 200, fadeTo = 0.5, topDistance = 30;
+		var topbarME = function() { $('#navigation').fadeTo(fadeSpeed, 1);} topbarML = function() {$('#navigation').fadeTo(fadeSpeed, fadeTo);};
+		var inside = false;
+		
+		$(window).scroll(function(){
+			position = $(window).scrollTop();
+			if(position > topDistance && !inside){
+				topbarML();
+				$('#navigation').bind('mouseenter', topbarME);
+				$('#navigation').bind('mouseleave',topbarML);
+				inside=true;
+			}
+			else if(position < topDistance){
+				topbarME();
+				$('#navigation').unbind('mouseenter', topbarME);
+				$('#navigation').unbind('mouseleave', topbarML);
+				inside=false;
+			}
+		});
+	})();
 });
